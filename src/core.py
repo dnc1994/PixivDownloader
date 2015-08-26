203511c56a0d090ad6dd3241b2ef5a984eaf25d1
Neo Zhang <zlhdnc1994@gmail.com>
2015-08-26 07:29:04 -0700

    daily commit

---
+++ src/core.py
@@ -0,0 +1,21 @@
+import requests
+import pyquery
+
+class Worker:
+	def __init__(self):
+
+	def login:
+	
+	def download:
+
+class Member:
+	def __init__(self):
+
+class Illust:
+	def __init__(self):
+
+class Manga:
+	def __init__(self):
+
+class Ugoriha:
+	def __init__(self):
